caa896cd11941871e256c89f69ff332d70510be1
luca pagano <lpagano@udesa.edu.ar>
2024-12-08 00:25:55 -0300

    fix: update vite config for proper asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/okrs/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  },
   server: {
     port: 3000,
   },
